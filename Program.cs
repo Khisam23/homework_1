4efe6b862ed707b1a2328e54c5c96217c4fe73b9
Ilmir Khisamov <khisamchick@yandex.ru>
2023-04-12 17:18:02 +0500

    Задача 1

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a > b){
+  System.Console.WriteLine("MAX = " + a);
+  System.Console.WriteLine("MIN = " + b);
+} else {
+  System.Console.WriteLine("MAX = " + b);
+  System.Console.WriteLine("MIN = " + a);
+}
