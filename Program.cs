cbf85ec563f6de14366d5f12700e0a25814366a6
Ilmir Khisamov <khisamchick@yandex.ru>
2023-04-13 11:36:46 +0500

    Задача 2

--- Program.cs
+++ Program.cs
@@ -1,13 +1,34 @@
-﻿Console.WriteLine("Введите первое число: ");
+﻿// Console.WriteLine("Введите первое число: ");
+// int a = int.Parse(Console.ReadLine());
+
+// Console.WriteLine("Введите второе число: ");
+// int b = int.Parse(Console.ReadLine());
+
+// if (a > b){
+//   System.Console.WriteLine("MAX = " + a);
+//   System.Console.WriteLine("MIN = " + b);
+// } else {
+//   System.Console.WriteLine("MAX = " + b);
+//   System.Console.WriteLine("MIN = " + a);
+// }
+
+
+Console.WriteLine("Введите первое число: ");
 int a = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Введите второе число: ");
+Console.WriteLine("Введите первое число: ");
 int b = int.Parse(Console.ReadLine());
 
-if (a > b){
-  System.Console.WriteLine("MAX = " + a);
-  System.Console.WriteLine("MIN = " + b);
-} else {
-  System.Console.WriteLine("MAX = " + b);
-  System.Console.WriteLine("MIN = " + a);
+Console.WriteLine("Введите первое число: ");
+int c = int.Parse(Console.ReadLine());
+
+if (a > b && a > c){
+  System.Console.WriteLine($"Максимальное число: {a}");
+} 
+else if (b > a && b > c){
+  System.Console.WriteLine($"Максимальное число: {b}");
+}
+else
+{
+  System.Console.WriteLine($"Максимальное число: {c}");
 }
